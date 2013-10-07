922e6fef9d1371e60c8afefabcecda23dcc7c357
Mark Chavez <los_populares@yahoo.com>
2013-10-08 01:35:04 +0800

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mrkjlchvz::Application.config.secret_token = 'ba1d8bc82e94abc0ba1102f3d1b7ef9c6e0fc4aeb96d7d65effe5ab3f4fa723fe7557ea9154b4c8a791ad7d36abb99ea457fca9444996af69c271aaa2c4a769d'
