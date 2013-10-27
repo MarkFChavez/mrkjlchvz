9cd56ef10eb765560696109150084dac32b22792
Mark Chavez <los_populares@yahoo.com>
2013-10-27 22:41:23 +0800

    changed public profile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
 
-  PROFILE = "http://ph.linkedin.com/pub/mark-chavez/33/2ab/5aa/"
+  PROFILE = "http://www.linkedin.com/pub/mark-chavez/33/2ab/5aa"
 
   def index
   	@profile = Linkedin::Profile.get_profile(PROFILE)
