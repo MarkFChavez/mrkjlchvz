afbe6f22d0203b85fc58331667bd9533261cbcc2
Mark Chavez <los_populares@yahoo.com>
2013-10-27 22:21:33 +0800

    Added linkedin-scraper

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.13'
 
 gem 'sqlite3'
 gem 'gravatar_image_tag'
+gem 'linkedin-scraper'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
+
+  PROFILE = "http://ph.linkedin.com/pub/mark-chavez/33/2ab/5aa/"
+
   def index
+  	@profile = Linkedin::Profile.get_profile(PROFILE)
   end
 end
